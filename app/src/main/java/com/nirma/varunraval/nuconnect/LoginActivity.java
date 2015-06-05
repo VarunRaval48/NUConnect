58fb6ca8549bb44921f969d5579d90469f7a84ea
Varun Raval <vraval48@gmail.com>
2015-06-05 15:05:16 +0530

    Final Login Update To get Userinfo

--- app/src/main/java/com/nirma/varunraval/nuconnect/LoginActivity.java
+++ app/src/main/java/com/nirma/varunraval/nuconnect/LoginActivity.java
@@ -2,21 +2,22 @@ package com.nirma.varunraval.nuconnect;
 
 import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.auth.UserRecoverableAuthException;
 import com.google.android.gms.common.AccountPicker;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -36,40 +37,46 @@ public class LoginActivity extends Activity{
     static final int REQUEST_CODE_PICK_ACCOUNT = 999;
     String email;
     String scope;
+    String oAuthscopes;
+    int REQUEST_AUTHORIZATION = 998, REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR = 997;
+    static boolean handling = false;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         scope = "audience:server:client_id:611036220045-sjstaa7r37ufc1t4q0iotb1otng8ktj2.apps.googleusercontent.com";
+        oAuthscopes = "oauth2:"+"https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/plus.login";
 
         if(!isDeviceOnline()){
             Toast.makeText(getApplicationContext(), "Connect to a network to Log in", Toast.LENGTH_LONG).show();
         }
         else{
+            Log.i("Picking", "Picking User Account");
             pickUserAccount();
-
         }
     }
 
     private void pickUserAccount(){
-        String accountTypes[] = new String[]{"com.google","in.ac.nirmauni"};
-        Intent in = AccountPicker.newChooseAccountIntent(null, null, accountTypes, false, null, null, null, null);
+        String accountTypes[] = new String[]{"com.google"}; //com.google.android.legacyimap for all types of accounts
+        Intent in = AccountPicker.newChooseAccountIntent(null, null, accountTypes, true, null, null, null, null);
 
         startActivityForResult(in, REQUEST_CODE_PICK_ACCOUNT);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        Log.i("Activity Result", "Request Code "+requestCode+" Result Code "+resultCode);
         if(requestCode == REQUEST_CODE_PICK_ACCOUNT){
             if(resultCode == RESULT_OK){
                 email = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
 
                 if(isDeviceOnline()){
-                    new GetUsername(LoginActivity.this, email, scope).execute();
+                    new GetUsername(LoginActivity.this, email, scope, oAuthscopes).execute();
                 }
             }
             else if(resultCode == RESULT_CANCELED){
                 Toast.makeText(this, "You must chose an account to Login", Toast.LENGTH_SHORT).show();
+                pickUserAccount();
             }
         }
     }
@@ -83,34 +90,54 @@ public class LoginActivity extends Activity{
         return false;
     }
 
+    public void handleException(final Exception e){
+        handling = true;
+        runOnUiThread(new Runnable() {
+            public void run() {
+                if (e instanceof GooglePlayServicesAvailabilityException) {
+                    int statusCode = ((GooglePlayServicesAvailabilityException) e)
+                            .getConnectionStatusCode();
+                    Dialog dialog = GooglePlayServicesUtil.getErrorDialog(statusCode,
+                            LoginActivity.this, REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
+                    dialog.show();
+                } else if (e instanceof UserRecoverableAuthException) {
+                    Intent intent = ((UserRecoverableAuthException) e).getIntent();
+                    startActivityForResult(intent, REQUEST_AUTHORIZATION);
+                }
+            }
+        });
+        handling = false;
+    }
 }
 
 class GetUsername extends AsyncTask{
 
-    Activity act;
+    LoginActivity act;
     String email;
     String scope;
+    String oAuthscopes;
 
-    GetUsername(Activity act, String email, String scope){
+    GetUsername(LoginActivity act, String email, String scope, String oAuthScopes){
         this.act = act;
         this.email = email;
         this.scope = scope;
+        this.oAuthscopes = oAuthScopes;
     }
 
     @Override
     protected Object doInBackground(Object[] params) {
-
         try {
-            String token = fetchToken();
-            Log.i("Token", token);
+            String idToken = fetchIDToken();
+            String accessToken = fetchAccessToken();
 
-            URL url = new URL("https://www.googleapis.com/oauth2/v3/tokeninfo?alt=json&id_token="+token);
+            while(act.handling){}
+
+            Log.i("ID Token", idToken);
+            Log.i("Access Token", accessToken);
+
+            URL url = new URL("https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token="+accessToken);
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-//            HttpClient httpClient = new DefaultHttpClient();
-//            HttpGet pageGet = new HttpGet(url.toURI());
-//            org.apache.http.HttpResponse hResponse = httpClient.execute(pageGet);
 
             InputStream iStream = conn.getInputStream();
 
@@ -140,10 +167,32 @@ class GetUsername extends AsyncTask{
         return null;
     }
 
-    protected String fetchToken() throws IOException{
+    protected String fetchIDToken() throws IOException{
         try {
             return GoogleAuthUtil.getToken(act, email, scope);
-        } catch (GoogleAuthException e) {
+        }
+        catch(GooglePlayServicesAvailabilityException e){
+            act.handleException(e);
+        }
+        catch(UserRecoverableAuthException e){
+            act.handleException(e);
+        }
+        catch (GoogleAuthException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    protected String fetchAccessToken() throws IOException{
+        try {
+            return GoogleAuthUtil.getToken(act, email, oAuthscopes);
+        }
+        catch(GooglePlayServicesAvailabilityException e){
+            act.handleException(e);
+        }
+        catch(UserRecoverableAuthException e){
+            act.handleException(e);
+        }
+        catch (GoogleAuthException e) {
             e.printStackTrace();
         }
         return null;
